d6fa23c0b1ce4a214988cde8ffc2ad431b6d31ed
Carla Cristina <carldepaula230@gmail.com>
2021-03-05 11:48:22 -0300

    Componentes home,get,post feitos e service gerado

--- FrontEnd/projetoAgendamento/src/app/app.module.ts
+++ FrontEnd/projetoAgendamento/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+import { GetComponent } from './get/get.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostComponent,
+    GetComponent
   ],
   imports: [
     BrowserModule,
