a09e786e369b09a4b6852630433c22088e9589ef
Carla Cristina <carldepaula230@gmail.com>
2021-03-05 16:09:03 -0300

    Função atualizar agendamento finalizada

--- FrontEnd/projetoAgendamento/src/app/app-routing.module.ts
+++ FrontEnd/projetoAgendamento/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
 {path: 'home', component: HomeComponent},
 {path: 'postar', component: PostComponent},
 {path: 'todos', component: GetComponent},
-{path: 'atualizar/:id', component: PutDeleteComponent},
+{path: 'atualizar', component: PutDeleteComponent},
 {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
