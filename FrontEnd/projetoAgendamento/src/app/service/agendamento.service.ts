39e1667e255443aab244f190e5680ab28f15b994
Carla Cristina <carldepaula230@gmail.com>
2021-03-05 17:21:44 -0300

    Deploy

--- FrontEnd/projetoAgendamento/src/app/service/agendamento.service.ts
+++ FrontEnd/projetoAgendamento/src/app/service/agendamento.service.ts
@@ -11,25 +11,25 @@ export class AgendamentoService {
 constructor(private http: HttpClient) { }
 
 buscarTodos() : Observable<Model[]>{
-  return this.http.get<Model[]>('http://localhost:8080/agendamento/status')
+  return this.http.get<Model[]>('https://desafio-agendamento.herokuapp.com/agendamento/status')
 }
 
 buscarUm(id:number) : Observable<Model>{
-  return this.http.get<Model>(`http://localhost:8080/agendamento/um/${id}`)
+  return this.http.get<Model>(`https://desafio-agendamento.herokuapp.com/agendamento/um/${id}`)
 }
 
 postAgendamento(model: Model): Observable<Model>{
-  return this.http.post<Model>('http://localhost:8080/agendamento/solicitacao', model)
+  return this.http.post<Model>('https://desafio-agendamento.herokuapp.com/agendamento/solicitacao', model)
 }
 
+
 putAgendamento(model: Model) : Observable<Model>{
-  return this.http.put<Model>('http://localhost:8080/agendamento/alterar', model)
+  return this.http.put<Model>('https://desafio-agendamento.herokuapp.com/agendamento/alterar', model)
 }
 
 deleteAgendamento(id: number) : Observable<Model>{
-  return this.http.delete<Model>(`http://localhost:8080/agendamento/${id}`)
+  return this.http.delete<Model>(`https://desafio-agendamento.herokuapp.com/agendamento/${id}`)
 }
-
 
 
 }
